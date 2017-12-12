00852d98b696b7f0c502f970dd0470f97ef8b696
Asad Razvi <debugme@hotmail.com>
2017-12-12 05:13:30 +0000

    master: Add Support For Users Route

---
+++ source/client/components/UsersList.jsx
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { fetchUsers } from '../actions/fetchUsers'
+
+class UsersList extends Component {
+  componentDidMount = () => {
+    this.props.fetchUsers()
+  }
+  render = () => {
+    const { users } = this.props
+    const usersList = this.renderUsers(users)
+    return <ul>{usersList}</ul>
+  }
+  renderUsers = (users) => {
+    const usersInfo = users.map(({ name }, index) => ({ key: `${name}${index}`, value: name}))
+    const usersList = usersInfo.map(({ key, value }) => <li key={key}>{value}</li>)
+    return usersList
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.users
+  }
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UsersList)
